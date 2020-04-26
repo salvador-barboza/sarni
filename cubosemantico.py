708bc84d2c9eda9f588845f828d458f6d7142912
Salvador Barboza <salvador.barboza.portillo@gmail.com>
2020-04-26 16:38:56 -0500

    * Se arreglo indentation  de cubo semantico

--- cubosemantico.py
+++ cubosemantico.py
@@ -1,24 +1,24 @@
 class CuboSemantico:
+  operando = {"int": 0,"float": 1, "char": 2,"bool": 3}
+  operador = {"+": 0,"-": 1,"/": 2,"*": 3,"<": 4,">": 5,"==": 6,"!=": 7,"&":8,"|":9}
 
-operando = {"int": 0,"float": 1, "char": 2,"bool": 3}
-operador = {"+": 0,"-": 1,"/": 2,"*": 3,"<": 4,">": 5,"==": 6,"!=": 7,"&":8,"|":9}
+  Cubo = [[["int","int","float","int","bool","bool","bool","bool","error","error"],
+          ["float","float","float","float","bool","bool","bool","bool","error","error"],
+          ["error","error","error","error","error","error","error","error","error","error"],
+          ["error","error","error","error","error","error","error","error","bool","bool"]],
+          [["float","float","float","float","bool","bool","bool","bool","error","error"],
+          ["float","float","float","float","bool","bool","bool","bool","error","error"],
+          ["error","error","error","error","error","error","error","error","error","error"],
+          ["error","error","error","error","error","error","error","error","error","error"]],
+          [["error","error","error","error","error","error","error","error","error","error"],
+          ["error","error","error","error","error","error","error","error","error","error"],
+          ["error","error","error","error","error","error","bool","bool","error","error"],
+          ["error","error","error","error","error","error","error","error","error","error"]],
+          [["error","error","error","error","error","error","error","error","bool","bool"],
+          ["error","error","error","error","error","error","error","error","error","error"],
+          ["error","error","error","error","error","error","error","error","error","error"],
+          ["error","error","error","error","error","error","bool","bool","bool","bool"]]
+  ]
 
-Cubo = [[["int","int","float","int","bool","bool","bool","bool","error","error"],
-        ["float","float","float","float","bool","bool","bool","bool","error","error"],
-        ["error","error","error","error","error","error","error","error","error","error"],
-        ["error","error","error","error","error","error","error","error","bool","bool"]],
-        [["float","float","float","float","bool","bool","bool","bool","error","error"],
-        ["float","float","float","float","bool","bool","bool","bool","error","error"],
-        ["error","error","error","error","error","error","error","error","error","error"],
-        ["error","error","error","error","error","error","error","error","error","error"]],
-        [["error","error","error","error","error","error","error","error","error","error"],
-        ["error","error","error","error","error","error","error","error","error","error"],
-        ["error","error","error","error","error","error","bool","bool","error","error"],
-        ["error","error","error","error","error","error","error","error","error","error"]],
-        [["error","error","error","error","error","error","error","error","bool","bool"],
-        ["error","error","error","error","error","error","error","error","error","error"],
-        ["error","error","error","error","error","error","error","error","error","error"],
-        ["error","error","error","error","error","error","bool","bool","bool","bool"]]
-        ]
-def typematch(self,op_izq,operador,op_der):
-  return self.Cubo[self.operando[op_izq]][self.operador[operador]][self.operando[op_der]]
+  def typematch(self,op_izq,operador,op_der):
+    return self.Cubo[self.operando[op_izq]][self.operador[operador]][self.operando[op_der]]
